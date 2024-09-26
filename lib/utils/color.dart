42ce3be4955848ca661d6fcd2bb90aada83af7db
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-09-27 03:35:39 +0500

    update

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -1,3 +1,11 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
-  
+ 
+
+
+  static const Color primary = Color(0x0ff00000);
+  static const Color secondary = Color(0xff86878d);
+  static const Color tertiary = Color(0xffbdbfc4);
+  static const Color quaternary = Color(0xffd3d5da);
 }
