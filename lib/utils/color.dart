08b006da31b2eb658b891cff44f044a5061801f5
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-09-28 04:36:06 +0500

    update

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -6,21 +6,31 @@ class AppColors {
   static const Color tertiary = Color(0xffbdbfc4);
   static const Color quaternary = Color(0xffd3d5da);
 
-  static const LinearGradient linerPrimary = LinearGradient(
+  static const LinearGradient linerGradientPrimary = LinearGradient(
     colors: [Color(0xff2E335A), Color(0xff1C1B33)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
 
-  static const LinearGradient linerSecondary = LinearGradient(
+  static const LinearGradient linerGradientSecondary = LinearGradient(
     colors: [Color(0xff5936B4), Color(0xff362A84)],
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
   );
-  
-  static const LinearGradient linerTertiary = LinearGradient(
+
+  static const LinearGradient linerGradientTertiary = LinearGradient(
     colors: [Color(0xff3658B1), Color(0xffC159EC)],
     begin: Alignment.centerLeft,
     end: Alignment.centerRight,
+  );
+  static const LinearGradient linerGradientQuaternary = LinearGradient(
+    colors: [Color(0xffAEC9FF), Color(0xff083072)],
+    begin: Alignment.topCenter,
+    end: Alignment.bottomCenter,
+  );
+
+  static const RadialGradient radialGradientPrimary = RadialGradient(
+    colors: [Color(0xffF7CBFD), Color(0xff7758D1)],
+    center: Alignment.bottomLeft,
   );
 }
---
+++ lib/utils/images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const String appIcon = 'assets/images/pictures/weather_app_logo.png';
+  static const String backgroundImage = 'assets/images/pictures/backgroundimage.png';
+  static const String house = 'assets/images/pictures/house.png';
+}
