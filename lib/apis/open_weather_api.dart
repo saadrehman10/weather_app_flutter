1813f1fecee631aa3c92db813f040f390d0b40fe
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-09-30 03:40:44 +0500

    update

--- lib/apis/open_weather_api.dart
+++ lib/apis/open_weather_api.dart
@@ -1,9 +1,18 @@
+import 'dart:convert';
+
+import 'package:weather_app_flutter/apis/api_keys.dart';
 import 'package:weather_app_flutter/models/current_weather.dart';
 import 'package:http/http.dart' as http;
 
 class OpenWeather {
-  Future<CurrentWeather> openWeatherApi({required String}) async {
-    String url = '';
-    final apiResponse = http.get(Uri.parse(url));
+  Future<CurrentWeather> openWeatherApi({required String city}) async {
+    String url =
+        'https://api.openweathermap.org/data/2.5/weather?appid=${ApiKeys.weatherAppKey}&q=$city&units=metric';
+    final apiResponse = await http.get(Uri.parse(url));
+    if (apiResponse.statusCode == 200) {
+      return CurrentWeather.fromJson(jsonDecode(apiResponse.body));
+    } else {
+      throw Exception('Response wasn\'t 200');
+    }
   }
 }
