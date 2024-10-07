f73b93a419c68241213a85b403c0b7ab3cb3ac1d
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-10-07 21:26:48 +0500

    update

--- lib/screens/main_screen/main_screen.dart
+++ lib/screens/main_screen/main_screen.dart
@@ -18,116 +18,114 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(image: AssetImage(AppImages.backgroundImage), fit: BoxFit.fill),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              TextField(
-                controller: _searchController,
-                focusNode: _searchFocus,
-                onChanged: (value) {
-                  setState(() {
-                    if (value == '' || value.isEmpty) {
-                      _currentCity = 'karachi';
-                    } else {
-                      _currentCity = value;
-                    }
-                  });
-                },
-                onTapOutside: (event) {
-                  _searchFocus.unfocus();
-                },
-                style: const TextStyle(color: AppColors.primary),
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  hintText: 'Search City',
-                  hintStyle: const TextStyle(color: AppColors.primary),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage(AppImages.backgroundImage), fit: BoxFit.fill),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            TextField(
+              controller: _searchController,
+              focusNode: _searchFocus,
+              onChanged: (value) {
+                setState(() {
+                  if (value == '' || value.isEmpty) {
+                    _currentCity = 'karachi';
+                  } else {
+                    _currentCity = value;
+                  }
+                });
+              },
+              onTapOutside: (event) {
+                _searchFocus.unfocus();
+              },
+              style: const TextStyle(color: AppColors.primary),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
                 ),
+                hintText: 'Search City',
+                hintStyle: const TextStyle(color: AppColors.primary),
               ),
-              FutureBuilder(
-                  future: OpenWeather.openWeatherApi(city: _currentCity),
-                  builder: (context, AsyncSnapshot<CurrentWeather> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Center(
-                        child: Padding(
-                          padding: EdgeInsets.all(30),
-                          child: Column(children: [
-                            CircularProgressIndicator(
+            ),
+            FutureBuilder(
+                future: OpenWeather.openWeatherApi(city: _currentCity),
+                builder: (context, AsyncSnapshot<CurrentWeather> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(30),
+                        child: Column(children: [
+                          CircularProgressIndicator(
+                            color: AppColors.primary,
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            MainPageText.loading,
+                            style: TextStyle(
                               color: AppColors.primary,
                             ),
-                            SizedBox(height: 20),
-                            Text(
-                              MainPageText.loading,
-                              style: TextStyle(
-                                color: AppColors.primary,
-                              ),
-                            ),
-                          ]),
-                        ),
-                      );
-                    } else if (snapshot.hasError) {
-                      return const Center(
-                        child: Text(
-                          'No City exist',
-                          style: TextStyle(
-                            color: AppColors.primary,
-                            fontSize: 100,
                           ),
+                        ]),
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return const Center(
+                      child: Text(
+                        'No City exist',
+                        style: TextStyle(
+                          color: AppColors.primary,
+                          fontSize: 100,
                         ),
-                      );
-                    } else if (snapshot.hasData) {
-                      final data = snapshot.data;
-                      return Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('${data?.main!.temp} °c',
+                      ),
+                    );
+                  } else if (snapshot.hasData) {
+                    final data = snapshot.data;
+                    return Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('${data?.main!.temp} °c',
+                              style: const TextStyle(
+                                color: AppColors.primary,
+                                fontSize: 110,
+                              )),
+                          Text(data?.weather![0].main ?? 'No main',
+                              style: const TextStyle(
+                                color: AppColors.primary,
+                                fontSize: 30,
+                              )),
+                          Text(_currentCity.capitalize,
+                              style: const TextStyle(
+                                color: AppColors.primary,
+                                fontSize: 50,
+                              )),
+                          Row(
+                            children: [
+                              Text(
+                                'H: ${data?.main!.tempMax.toString() ?? 'no max tep'} °c',
                                 style: const TextStyle(
                                   color: AppColors.primary,
-                                  fontSize: 110,
-                                )),
-                            Text(data?.weather![0].main ?? 'No main',
-                                style: const TextStyle(
-                                  color: AppColors.primary,
-                                  fontSize: 30,
-                                )),
-                            Text(_currentCity.capitalize,
-                                style: const TextStyle(
-                                  color: AppColors.primary,
-                                  fontSize: 50,
-                                )),
-                            Row(
-                              children: [
-                                Text(
-                                  'H: ${data?.main!.tempMax.toString() ?? 'no max tep'} °c',
-                                  style: const TextStyle(
-                                    color: AppColors.primary,
-                                    fontSize: 20,
-                                  ),
+                                  fontSize: 20,
                                 ),
-                                const SizedBox(width: 10),
-                                Text(
-                                  'L: ${data?.main!.tempMin.toString() ?? 'no min tep'} °c',
-                                  style: const TextStyle(
-                                    color: AppColors.primary,
-                                    fontSize: 20,
-                                  ),
+                              ),
+                              const SizedBox(width: 10),
+                              Text(
+                                'L: ${data?.main!.tempMin.toString() ?? 'no min tep'} °c',
+                                style: const TextStyle(
+                                  color: AppColors.primary,
+                                  fontSize: 20,
                                 ),
-                              ],
-                            ),
-                          ]);
-                    } else {
-                      return const Placeholder();
-                    }
-                  }),
-            ]),
-          ),
+                              ),
+                            ],
+                          ),
+                        ]);
+                  } else {
+                    return const Placeholder();
+                  }
+                }),
+          ]),
         ),
       ),
     );
