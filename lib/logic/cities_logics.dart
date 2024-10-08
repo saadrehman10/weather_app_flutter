4e0d693b9fd0215d1e528c0212860b306c9157b4
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-10-08 23:45:01 +0500

    update

--- lib/logic/cities_logics.dart
+++ lib/logic/cities_logics.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CitiesLogics {
- 
+  static Future<void> setCurrentDisplayedCity({required String city}) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    sp.setString('displayedCity', city);
+  }
+
+  static Future<String> getCurrentDisplayedCity() async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    String? displayedCity = sp.getString('displayedCity');
+    if (displayedCity == null) {
+      setCurrentDisplayedCity(city: 'karachi');
+      return 'karachi';
+    } else {
+      return displayedCity;
+    }
+  }
+  
+  
 
 }
