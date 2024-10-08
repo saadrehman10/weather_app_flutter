71b5838a41b33c15ca83313d636d998c588a6ae7
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-10-09 00:18:57 +0500

    update

--- lib/logic/cities_logics.dart
+++ lib/logic/cities_logics.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CitiesLogics {
@@ -32,14 +33,23 @@ class CitiesLogics {
   static Future<void> addUserAddedCityList({required String addCity}) async {
     SharedPreferences sp = await SharedPreferences.getInstance();
     List<String> cityList = await getUserAddedCityList();
-    cityList.add(addCity);
-    sp.setStringList('userAddedCities', cityList);
+    if (cityList.contains(addCity)) {
+      debugPrint('city already exist');
+    } else {
+      cityList.add(addCity);
+      sp.setStringList('userAddedCities', cityList);
+    }
   }
 
   static Future<void> deleteUserAddedCityList({required String deleteCity}) async {
     SharedPreferences sp = await SharedPreferences.getInstance();
     List<String> cityList = await getUserAddedCityList();
-    cityList.remove(deleteCity);
-    sp.setStringList('userAddedCities', cityList);
+    if (cityList.contains(deleteCity)) {
+      cityList.remove(deleteCity);
+      sp.setStringList('userAddedCities', cityList);
+    } else {
+      debugPrint('Deleted city don\'t exist');
+      throw Exception('Deleted city don\'t exist');
+    }
   }
 }
