09a2a183abc136cdbe37e3ce87f08264aef8b8e6
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-10-09 20:25:44 +0500

    update

--- lib/logic/cities_logics.dart
+++ lib/logic/cities_logics.dart
@@ -63,7 +63,45 @@ class CitiesLogics {
 
   static Future<Map<String, dynamic>> getOldDataForWhileLoading() async {
     SharedPreferences sp = await SharedPreferences.getInstance();
-    final Map<String, dynamic> jsonDecodedData = jsonDecode(sp.getString('oldData')!);
-    return jsonDecodedData;
+    final String? oldData = sp.getString('oldData');
+    if (oldData == null) {
+      Map<String, dynamic> temp = {
+        "coord": {"lon": 67.0822, "lat": 24.9056},
+        "weather": [
+          {"id": 721, "main": "Haze", "description": "haze", "icon": "50n"}
+        ],
+        "base": "stations",
+        "main": {
+          "temp": 34.9,
+          "feels_like": 33.32,
+          "temp_min": 34.9,
+          "temp_max": 34.9,
+          "pressure": 1008,
+          "humidity": 23,
+          "sea_level": 1008,
+          "grnd_level": 1005
+        },
+        "visibility": 4000,
+        "wind": {"speed": 5.14, "deg": 170},
+        "clouds": {"all": 75},
+        "dt": 1728480758,
+        "sys": {
+          "type": 1,
+          "id": 7576,
+          "country": "PK",
+          "sunrise": 1728437215,
+          "sunset": 1728479435
+        },
+        "timezone": 18000,
+        "id": 1174872,
+        "name": "Karachi",
+        "cod": 200
+      };
+      setOldDataForWhileLoading(saveData: temp);
+      return temp;
+    } else {
+      final Map<String, dynamic> jsonDecodedData = jsonDecode(oldData);
+      return jsonDecodedData;
+    }
   }
 }
